be793535f11360ad2a7b279d7b6c6a1028b190d5
litchichi <136559829+litchichi@users.noreply.github.com>
2023-11-18 00:57:24 +0800

    Update chw5.c

--- chw5.c
+++ chw5.c
@@ -96,6 +96,7 @@ void checkNewStepByColor(int next[8][8], int color)
     }
     for (int i = 0; i < 8; i++) {
         for (int j = 0; j < 8; j++) {
+            if(chess[i][j] == 0)
             checkNewStep(next, i, j, color);
         }
     }
