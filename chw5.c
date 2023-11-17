a77786c733b2b138417882ff2059a294a7305c3e
litchichi <136559829+litchichi@users.noreply.github.com>
2023-11-17 22:37:21 +0800

    Update chw5.c

--- chw5.c
+++ chw5.c
@@ -1,99 +1,141 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 
-void checkNewStep(int chess[8][8], int next[8][8], int x, int y);
+void printChessBoard();
+void makeMove(int x, int y, int color);
+int getBestMove(int next[8][8], int color);
+void checkNewStepByColor(int next[8][8], int color);
+void checkNewStep(int next[8][8], int x, int y, int color);
+int newStep(int x, int y, int dx, int dy, int color);
 
-int chess[8][8] = { {0,0,0,0,0,0,0,0},
-                 {0,0,0,0,0,0,0,0},
-                 {0,0,0,0,0,0,0,0},
-                 {0,0,0,1,2,0,0,0},
-                 {0,0,0,2,1,0,0,0},
-                 {0,0,0,0,0,0,0,0},
-                 {0,0,0,0,0,0,0,0},
-                 {0,0,0,0,0,0,0,0}
+int chess[8][8] = {
+    {0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 1, 2, 0, 0, 0},
+    {0, 0, 0, 2, 1, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0}
 };
 
-void checkNewStepByColor(int chess[8][8], int next[8][8], int color)
-{
-    for (int i = 0; i < 8; i++) {
-        for (int j = 0; j < 8; j++) {
-            if (chess[i][j] == color) {
-                checkNewStep(chess, next, i, j);
-            }
-        }
-    }
-}
+int directions[8][2] = { {-1, -1},{-1, 0},{-1, 1},{0, -1},{0, 1},{1, -1},{1, 0},{1, 1} };
 
-void checkNewStep(int chess[8][8], int next[8][8], int x, int y) {
-    int directions[8][2] = {{-1, -1},{-1, 0},{-1, 1},{0, -1},{0, 1},{1, -1},{1, 0},{1, 1}};
-
-    for (int k = 0; k < 8; k++) {
-        int dx = directions[k][0];
-        int dy = directions[k][1];
-        int step = newStep(chess, x, y, dx, dy);
-        if (step > 0) {
-            int i = x + step * dx;
-            int j = y + step * dy;
-            next[i][j] = step;
-        }
-    }
-}
-
-int newStep(int chess[8][8], int x, int y, int dx, int dy)
-{
-    int i = x + dx;
-    int j = y + dy;
-    int n = 1;
-
-    while (i >= 0 && i < 8 && j >= 0 && j < 8) {
-        if (chess[i][j] == 0) {
-            return n;
-        }
-        i += dx;
-        j += dy;
-        n++;
-    }
+int main() {
+    int chess[8][8], next[8][8], color, x, y;
+ 
+    printf("1 黑棋你下\n");
+    printChessBoard();
+    checkNewStepByColor(next, 1);
+    printf("請下合法的位置 x y\n");
+    scanf("%d %d", &x, &y);
+    makeMove(x, y, 1);
+    printf("2 白棋電腦下\n");
+    printChessBoard();
+    x, y = getBestMove(next, 2);
+    makeMove(x, y, 2);
+    printf("電腦下 (%d,%d)\n");
+    printChessBoard();
 
     return 0;
 }
 
-int main() {
-    int chess[8][8],color=1,next[8][8]={0},x,y;
+void printChessBoard() {
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 8; j++) {
+            printf("%d ", chess[i][j]);
+        }
+        printf("\n");
+    }
+}
 
-    while (1) {
-        printf("黑子可下位置:\n");
-        checkNewStepByColor(chess, next, 2);
-        scanf("%d %d", &x, &y);
-        checkNewStep(chess, next, x, y);
-        printf("白子可下位置:\n");
-        checkNewStepByColor(chess, next, 1);
-        printf("黑子可下位置:\n");
-        checkNewStepByColor(chess, next, 2);
+void makeMove(int x, int y, int color) {
+    chess[x][y] = color;
+    for (int k = 0; k < 8; k++) {
+        int dx = directions[k][0];
+        int dy = directions[k][1];
+        int flips = newStep(x, y, dx, dy, color);
+        for (int i = 1; i <= flips; i++) {
+            chess[x + i * dx][y + i * dy] = color;
+        }
+    }
+}
+ 
+int getBestMove(int next[8][8], int color) {
+    int max_x = -1;
+    int max_y = -1;
+    int max_flips = 0;
 
-        int count = 0, blackCount = 0,whiteCount = 0;
-        for (int x = 0; x < 9; x++) {
-            for (int y = 0; y < 9; y++) {
-                if (chess[x][y] == 1) {
-                    whiteCount++;
-                    count++;
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 8; j++) {
+            if (next[i][j] > 0) {
+                if (next[i][j] > max_flips) {
+                    max_x = i;
+                    max_y = j;
+                    max_flips = next[i][j];
                 }
-                else if (chess[x][y] == 1) {
-                    blackCount++;
-                    count++;
-                }
-                else;
             }
         }
-        if (count == 64) {
-            if (whiteCount > blackCount)
-                printf("白棋獲勝!\n");
-            else if (whiteCount < blackCount)
-                printf("黑棋獲勝!\n");
-            else
-                printf("平手!\n");
-            break;
-        }
-            
     }
-	return 0;
+
+    return (max_x, max_y);
+}
+
+
+
+void checkNewStepByColor(int next[8][8], int color)
+{
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 8; j++) {
+            next[i][j] = 0;
+        }
+    }
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 8; j++) {
+            checkNewStep(next, i, j, color);
+        }
+    }
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 8; j++) {
+            if (next[i][j] > 0) {
+                printf("(%d,%d) ", i, j);
+            }
+        }
+    }
+    printf("\n");
+}
+
+
+void checkNewStep(int next[8][8], int x, int y, int color) {
+    for (int k = 0; k < 8; k++) {
+        int dx = directions[k][0];
+        int dy = directions[k][1];
+        int n = newStep(x, y, dx, dy, color);
+        if (n > 0) {
+            next[x][y] = n;
+        }
+    }
+}
+
+int newStep(int x, int y, int dx, int dy, int color)
+{
+    int X = x + dx;
+    int Y = y + dy;
+    int n = 1;
+
+    if (X >= 0 && X < 8 && Y >= 0 && Y < 8) {
+        if (chess[X][Y] != color && chess[X][Y] != 0) {
+            while (X >= 0 && X < 8 && Y >= 0 && Y < 8) {
+                if (chess[X][Y] == 0) {
+                    break;
+                }
+                else if (chess[X][Y] == color) {
+                    return n;
+                }
+                X += dx;
+                Y += dy;
+                n++;
+            }
+        }
+    }
+    return 0;
 }
